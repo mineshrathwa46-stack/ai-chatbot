48c7e0963fde913f23e08f54b3e67c43a3fa65b9
mineshrathwa46-stack <mineshrathwa46@gmail.com>
2026-08-10 14:19:22 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:5000",
+  baseURL: import.meta.env.VITE_BACKEND_URL || "ai-chatbot-backend-74c8.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
